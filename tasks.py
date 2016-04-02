e27b12a8817fc39017410559564f09e9e7f605c2
jhermann <jh@web.de>
2016-04-02 19:06:01 +0200

    Start preview in a clean shell, and stop any left-over watchdog

--- tasks.py
+++ tasks.py
@@ -9,10 +9,15 @@ import os
 import sys
 import shlex
 import shutil
+import subprocess
 
 from rituals.easy import task, Collection, pushd
 from rituals.util import antglob, notify
 from rituals.acts.documentation import namespace as _docs
+
+ROOT_FOLDER = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(ROOT_FOLDER, "tests"))
+from conftest import TEST_FOLDER
 
 
 @task(help=dict(
@@ -23,9 +28,13 @@ def clean(ctx, venv=False, extra=''):
     """Perform house-keeping."""
     notify.banner("Cleaning up project files")
 
+    # Stop any left-over watchdog
+    if os.path.exists(TEST_FOLDER):
+        ctx.run("cd {} && . .env && inv view --kill".format(TEST_FOLDER))
+
     # Add patterns based on given parameters
     venv_dirs = ['.venv']
-    patterns = ['hovercraft-presentation-slides/', 'pip-selfcheck.json', '**/*~']
+    patterns = [TEST_FOLDER + '/', 'pip-selfcheck.json', '**/*~']
     if venv:
         patterns.extend([i + '/' for i in venv_dirs])
     if extra:
@@ -51,9 +60,13 @@ def clean(ctx, venv=False, extra=''):
 def test(ctx):
     """Perform integration tests."""
     ctx.run("py.test")
-    with pushd('hovercraft-presentation-slides'):
+    with pushd(TEST_FOLDER):
         assert os.path.exists('README.rst'), "README is created"
-        ctx.run(". .env --yes && inv view")
+        ctx.run(". .env --yes")
+
+    # Start preview in a clean shell environment
+    subprocess.call('''bash -l -c "cd '{}/{}' && . .env && invoke view"'''
+                    .format(os.getcwd(), TEST_FOLDER), shell=True)
 
 
 namespace = Collection.from_module(sys.modules[__name__], name='')
